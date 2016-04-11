beb5767de6cb321a9653f95b2cc03bc57b5d9d98
Salvatore <salvatore.stefanelli@ymail.com>
2016-04-11 11:53:08 +0000

    test staging

--- reader.py
+++ reader.py
@@ -4,7 +4,7 @@ from pandas_datareader.data import DataReader
 symbols_list = ['ORCL', 'TSLA', 'IBM','YELP', 'MSFT']
 d = {}
 for ticker in symbols_list:
-    d[ticker] = DataReader(ticker, "yahoo", '2016-01-01')
+    d[ticker] = DataReader(ticker, "yahoo", '2016-03-01')
 pan = pd.Panel(d)
 df1 = pan.minor_xs('Adj Close')
 print(df1)
